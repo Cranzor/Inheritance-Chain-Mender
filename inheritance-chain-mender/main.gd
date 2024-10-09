5aba12b086251b65929e12851ecfe5fe0568d8b0
Jesse <jessew9602@gmail.com>
2024-10-09 23:52:54 +0900

    Implemented replacement of 'extends' + class name with file path instead

--- inheritance-chain-mender/main.gd
+++ inheritance-chain-mender/main.gd
@@ -1,16 +1,19 @@
 @tool
 extends EditorScript
 
+var classes
+var classes_array
+var overwrite_files = false
+var classes_with_file_path: Dictionary = {}
 
 # Called when the script is executed (using File -> Run in Script Editor).
 func _run() -> void:
 	#print(collision_grid.find_grid_position(Vector2(192, 160), Vector2(16, 16)))
 	#alarm.start(10)
 	#dir_contents("res://")
-	var classes = ProjectSettings.get_global_class_list()
+	classes = ProjectSettings.get_global_class_list()
+
 	scan_dir("res://")
-	
-	var classes_with_file_path: Dictionary = {}
 	
 	for class_instance in classes:
 		classes_with_file_path[class_instance['class']] = class_instance['path']
@@ -52,17 +55,28 @@ func scan_dir(path):
 
 func comment_out_class_name(file):
 	var class_name_regex = RegEx.new()
-	class_name_regex.compile("(?m)^(class_name +.*)")
+	class_name_regex.compile("(?m)^(class_name +.+)")
 	
 	var opened_file = FileAccess.open(file, FileAccess.READ)
 	var content = opened_file.get_as_text()
 	
 	var after_search = class_name_regex.sub(content, "#$1")
-	print(after_search)
+	#print(after_search)
 	
-	opened_file = FileAccess.open(file, FileAccess.WRITE)
+	var extends_regex = RegEx.new()
+	extends_regex.compile("(?m)^(extends) +(.*)")
 	
-	opened_file.store_string(after_search)
+	var search = extends_regex.search(content)
+	var class_check = search.strings
+	if classes_with_file_path.has(str(class_check[2])):
+		print("hi")
+		var after_after_search = extends_regex.sub(after_search, str(class_check[1]) + " '" + str(classes_with_file_path[class_check[2]]) +"'")
+		print(after_after_search)
+	
+	#--------------------------------------------------	
+	if overwrite_files == true:
+		var again_opened_file = FileAccess.open(file, FileAccess.WRITE)
+		again_opened_file.store_string(after_search)
 	
 	#var result = class_name_regex.search(content)
 	#if result:
